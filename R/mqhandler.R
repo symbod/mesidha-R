b78e1b685a333122005eb998963c84e4fbefb899
kadam0 <k.adamowicz@tum.de>
2022-11-08 14:56:08 +0100

    add reduce gene

--- R/mqhandler.R
+++ R/mqhandler.R
@@ -10,7 +10,8 @@ suppressPackageStartupMessages({
   }
 })
 ## Load python library ----
-py_install("mqhandler", pip = TRUE)
+py_install("pip", ignore_installed=TRUE)
+py_install("mqhandler", pip = TRUE, ignore_installed=TRUE)
 if (!py_module_available("mqhandler")) {
   stop("Python package could not be installed")
 }
@@ -31,8 +32,8 @@ mq <- import("mqhandler")
 #' @param protein_column name of column with protein IDs
 #' @param organism (optional) specify organism the ids should match to
 #' @param decoy (optional) bool to indicate if protein ids from decoy fasta (REV__, CON__) should be kept
-#' @param action (optional) what to do, if IDs cell is empty after filtering. Keep empty cell, delete it or fill it based on gene name.
-#' @param gene_column (optional) name of column with gene names
+#' @param res_column name of column of filtered protein IDs. If NULL, the protein_column will be overridden
+#' @param keep_empty bool to indicate if empty reduced gene names cells should be kept or deleted
 #' @param reviewed (optional) bool to indicate if newly retrieved protein IDs should be reduced to reviewed ones
 #'
 #' @return filtered Dataframe
@@ -40,11 +41,11 @@ mq <- import("mqhandler")
 #'
 #' @examples
 filter_protein_ids <- function(data, protein_column, organism = NULL, decoy = FALSE, 
-                               action = "delete", gene_column = NULL, reviewed = TRUE) {
+                               keep_empty = FALSE, res_column = NULL, reviewed = TRUE) {
   
-  return(mq$filter_ids$filter_protein_ids(data = data, id_column = protein_column, 
+  return(mq$filter_ids$filter_protein_ids(data = data, protein_column = protein_column, 
                                           organism = organism, decoy = decoy, 
-                                          action = action, gene_column = gene_column,
+                                          keep_empty = keep_empty, res_column = res_column,
                                           reviewed = reviewed))
 }
 
@@ -66,7 +67,8 @@ filter_protein_ids <- function(data, protein_column, organism = NULL, decoy = FA
 #' @param mode mode of refilling (See in description for more infos)
 #' @param protein_column name of column with protein IDs
 #' @param gene_column (optional) name of column with gene names
-#' @param skip_filled (optional) bool to indicate if already filled gene names should be skipped
+#' @param res_column name of column of reduced gene names results. If NULL, the gene_column will be overridden
+#' @param keep_empty bool to indicate if empty reduced gene names cells should be kept or deleted
 #' @param organism (optional) specify organism the ids should match to
 #' @param fasta (optional) fasta file when mode all or fasta
 #'
@@ -74,11 +76,43 @@ filter_protein_ids <- function(data, protein_column, organism = NULL, decoy = FA
 #' @export
 #'
 #' @examples
-remap_genenames <- function(data, mode, protein_column, gene_column = NULL,
+remap_genenames <- function(data, mode, protein_column, gene_column, res_column = NULL,
                             skip_filled = FALSE, organism = NULL, fasta = NULL){
   return(mq$remap_genenames$remap_genenames(data = data, mode = mode, protein_column = protein_column,
-                                            gene_column = gene_column, skip_filled = skip_filled, 
-                                            organism = organism, fasta = fasta))
+                                            gene_column = gene_column, res_column = res_column,
+                                            skip_filled = skip_filled, organism = organism, fasta = fasta))
+}
+
+## Reduce Gene Names ----
+
+#' @title Reduce Gene Names
+#' 
+#' Use existing Gene Names to reduce them and replace them by a single Name 
+#' to redundant Names. Following modes are possible.
+#' 
+#' Modes:
+#' ensembl - Use gProfiler to reduce gene names to those having a Ensembl ID
+#' HGNC - Use HGNC database to reduce gene names to those having an entry in HGNC (only for human)
+#' mygeneinfo - Use mygeneinfo database to reduce gene names to those having an entry in mygeneinfo
+#' enrichment - Use gProfiler to reduce gene names to those having a functional annotation
+#'
+#' @param data a Dataframe with a column containing the protein IDs
+#' @param mode mode of refilling (See in description for more infos)
+#' @param gene_column name of column with gene names
+#' @param res_column name of column of reduced gene names results. If NULL, the gene_column will be overridden
+#' @param keep_empty bool to indicate if empty reduced gene names cells should be kept or deleted
+#' @param organism (optional) specify organism the ids should match to
+#' @param HGNC_mode setting on how to selected the gene names in HGNC (mostfrequent, all)
+#'
+#' @return reduced Dataframe
+#' @export
+#'
+#' @examples
+remap_genenames <- function(data, mode, gene_column, organism, 
+                            res_column = NULL, keep_empty = FALSE, HGNC_mode = "mostfrequent"){
+  return(mq$reduce_genenames$reduce_genenames(data = data, mode = mode, gene_column = gene_column,
+                                              res_column = res_column, keep_empty = keep_empty, 
+                                              organism = organism, HGNC_mode = HGNC_mode))
 }
 
 ### Get Orthologs ----
@@ -91,14 +125,18 @@ remap_genenames <- function(data, mode, protein_column, gene_column = NULL,
 #' @param gene_column name of column with gene names
 #' @param organism specify organism the ids currently match to
 #' @param tar_organism specify organism the ids should match to
+#' @param res_column name of column of reduced gene names results. If NULL, the gene_column will be overridden
+#' @param keep_empty bool to indicate if empty reduced gene names cells should be kept or deleted
 #'
 #' @return dataframe with orthologs
 #' @export
 #'
 #' @examples
-get_orthologs <- function(data, gene_column, organism, tar_organism) {
-  return(mq$map_orthologs$get_orthologs(data = data, gene_column = gene_column, 
-                                        organism = source_organism, tar_organism = target_organism))
+get_orthologs <- function(data, gene_column, organism, tar_organism,
+                          res_column, keep_empty) {
+  return(mq$map_orthologs$map_orthologs(data = data, gene_column = gene_column, 
+                                        organism = source_organism, tar_organism = target_organism,
+                                        res_column = res_column, keep_empty = keep_empty))
 }
 
 ## Smaller functions ----
